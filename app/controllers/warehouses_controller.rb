50d726e42f50985e42f4eea7b58fd9f08a96d824
Mariano Sobrinho <onairam17@gmail.com>
2024-04-14 11:00:03 -0400

    Formulario de Cadastro de um galpão - Aula 14

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -1,5 +1,20 @@
 class WarehousesController < ApplicationController
-    def show
-        @warehouse =  Warehouse.find(params[:id])
-    end
+  def show
+    @warehouse =  Warehouse.find(params[:id])
+  end
+    
+  def new
+  end
+
+  def create
+#    1 - Recebe os dados enviados 
+    warehouse_params = params.require(:warehouse).permit( :name, :code, :city, :address, :description, :cep, :area)
+    
+#    2 - Cria um novo galpão no banco de dados
+    w=Warehouse.new(warehouse_params)
+    w.save()
+#    3 - Redireciona para a tela inicial
+    redirect_to root_path
+  end
+
 end
