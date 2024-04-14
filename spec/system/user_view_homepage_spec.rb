381f478bab840538126844c1237809e2d63aaed3
Mariano Sobrinho <onairam17@gmail.com>
2024-04-13 23:19:35 -0400

    Listando Galpões na tela inicial -  Aula 8

--- spec/system/user_view_homepage_spec.rb
+++ spec/system/user_view_homepage_spec.rb
@@ -7,6 +7,24 @@ describe 'Usuario visita tela inicial' do
     visit('/')
     # Assert
     expect(page).to have_content('Galpões & Estoque')
+  end 
 
+  it 'e vê os galpões cadrastrados' do
+    # Arrange
+    Warehouse.create(name: 'Rio', code: 'SDU',  city: 'Rio de Janeiro', area: '60000')
+    Warehouse.create(name: 'Maceio', code: 'MCZ',  city: 'Maceio', area: '50000')
+
+    # Act
+    visit('/')
+    # Assert
+    expect(page).to have_content('Rio')
+    expect(page).to have_content('SDU')
+    expect(page).to have_content('Rio de Janeiro')
+    expect(page).to have_content('60000 m²')
+
+    expect(page).to have_content('Maceio')
+    expect(page).to have_content('MCZ')
+    expect(page).to have_content('Maceio')
+    expect(page).to have_content('50000 m²')
   end 
 end
